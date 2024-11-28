0d6c7b55b4e6e0211466d525de38527accc90c97
test <test@gamil.com>
2024-11-28 18:15:29 +0100

    crud dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'crud';
